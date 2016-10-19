5e26d1fde0ff160813f863bfc5c15f880fd14e01
Jo Colina <jo@jocolina.com>
2016-10-19 09:29:30 +0200

    ThemeManager Corrections

--- models/themeManager.js
+++ models/themeManager.js
@@ -1,41 +1,52 @@
 var ThemeManager = {};
 
-
 var models = require('./models.js');
 
 var fs = require('fs');
 
 ThemeManager.themes = [];
 
-
-ThemeManager.chargerJson = function(){
-    fs.readFile('./dico.json', 'utf8', function (err, data) {
-	if (err)
-	{
-	    console.log('Erreur chargement json');
-	    return;
-	}
-	else
-	{
-	    console.log('Json charge');
-	    ThemeManager.themes = JSON.parse(data);
-	}
-	
+// noms toujours en ANGLAIS
+ThemeManager.loadThemes = function(){
+	var data = null;
+    try {
+    	// on utilise la methode SYNCRHONE
+    	// c'est un chargement de config au debut
+    	// du programme
+    	data = fs.readFileSync('./dico.json', 'utf8');
+    	data = JSON.parse(data);
+    	ThemeManager.themes = data;
+    } catch(e) {
+	    console.error('Erreur chargement json', e);
     }
-	       )}
+
+};
 
 ThemeManager.loadTheme = function(id){
 
     for(var i=0;i<ThemeManager.themes.length;i++)
     {
-	if(ThemeManager.themes[i].name==id)
-	{
-	    return ThemeManager.themes[i];
-	}
+    	// indentation!!
+		if(ThemeManager.themes[i].name==id)
+		{
+			return ThemeManager.themes[i];
+		}
 	
     }
-    console.log('Theme non trouvé');
-    return;
+    console.error('Theme non trouvé');
+    // retourner une valeur quand meme
+    // histoire de pouvoir comparer 
+    // quand on utilise la methode
+    // ex: (if(TM.loadTheme(5) == null))...
+    return null;
+};
+
+ThemeManager.newTheme = function(name, dict, tt, ct){
+	var t = new models.Theme(name);
+	var d = new Dictionary(dict, tt, ct);
+	t.dictionary = d;
+	ThemeManager.themes.push(t);
+	return t;
 };
 
 
